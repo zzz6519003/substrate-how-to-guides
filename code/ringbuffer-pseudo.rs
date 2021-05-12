e67b19651968436af39d44710720c9cf965e0fb9
Sacha <23283108+sacha-l@users.noreply.github.com>
2021-05-12 19:43:06 -0400

    Deploy website - based on d54eba6a5a7f9a0eb6ba844e2301b1015c18bab1

---
+++ code/ringbuffer-pseudo.rs
@@ -0,0 +1,19 @@
+// Pseudo code: use transient storage adapter
+
+// First we define a constructor function, `queue_transient`, 
+// so we don't have to specify the types every time we want
+// to access the transient. This function constructs a
+// ringbuffer transient and returns it as a boxed trait
+// object.
+
+// Create the `add_multiple` function shows the 
+// actual typical usage of our transient.
+
+// In `add_multiple`, use the `queue_transient` 
+// function defined above to get a `queue` object.
+
+// Then, `push` into the queue repeatedly with 
+// `commit` happening at the end of the function, 
+// upon `drop` of the `queue` object. 
+// `pop` works analogously and could also be intermixed 
+// with `push`
